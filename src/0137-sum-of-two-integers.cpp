304f69f266a68631ca88db1146a159d8f5935046
Ashish Misra <amisra@salesforce.com>
2016-10-04 13:05:35 -0700

    Moved hello-world to new name.

---
+++ src/0137-sum-of-two-integers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+		return 0 ;
+    }
+};
+
+int main(int argc, const char **argv) {
+
+	string output("Hello world!!!");
+
+	cout << output << endl;
+}
