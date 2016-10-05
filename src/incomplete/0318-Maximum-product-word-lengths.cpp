446eed7256ec87c9f8eb88f91495e6d97c579be3
Ashish Misra <amisra@salesforce.com>
2016-10-05 16:12:37 -0700

    Marginally modified solution for problem 0318. Algorithm is still incorrect though.

--- src/incomplete/0318-Maximum-product-word-lengths.cpp
+++ src/incomplete/0318-Maximum-product-word-lengths.cpp
@@ -11,19 +11,14 @@ using std::vector;
 #include <bitset>
 using std::bitset;
 
+static const int NUM_CHARS = 26;
+
 class Solution {
 	public:
     	int maxProduct(vector<string>& words);
 };
 
-bool shareChars(const string &word1, const string &word2) {
-	const int NUM_CHARS = 26;
-	bitset<NUM_CHARS> char_map;
-
-	for(int i=0; i < word1.size(); i++) {
-		int pos = word1[i] - 'a';
-		char_map.set(pos);
-	}
+bool shareChars(bitset<NUM_CHARS> &char_map, const string &word2) {
 
 	for(int i=0; i < word2.size(); i++) {
 		int pos = word2[i] - 'a';
@@ -36,13 +31,33 @@ bool shareChars(const string &word1, const string &word2) {
 
 int Solution::maxProduct(vector<string>& words) {
 	
+
 	int product = 0;
+	vector<int> word_size(words.size());
+	vector<bitset<NUM_CHARS> > word_char_map(words.size());
+
+	// Precomputing size and character-sets data for each word.
+	for (int i=0; i < words.size(); i++) {
+
+		string &curr_word = words[i];
+		bitset<NUM_CHARS> &curr_map = 	word_char_map[i];
+
+		word_size[i] = curr_word.length();
+
+		for(int j=0; j < curr_word.size(); j++) {
+			int pos = curr_word[j] - 'a';
+			curr_map.set(pos);
+		}
+	}
 
 	for(int i=0 ; i < words.size(); i++) {
+
+		bitset<NUM_CHARS> &curr_map = 	word_char_map[i];
+
 		for(int j=i+1; j < words.size(); j++) {
-			if (shareChars(words[i], words[j]) == false) {
-				
-				int new_prod = words[i].size() * words[j].size() ;
+
+			if (shareChars(curr_map, words[j]) == false) {
+				int new_prod = word_size[i] * word_size[j] ;
 				if (new_prod > product) {
 					product = new_prod;
 				}
@@ -55,7 +70,14 @@ int Solution::maxProduct(vector<string>& words) {
  
 int main(int argc, const char **argv) {
 
-	string output("Hello world!!!");
+	Solution ans;
 
-	cout << output << endl;
+	const char *arr1[] = {"abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
+	vector<string> vec1;
+	for(int i = 0; i < 6; i++) {
+		vec1.push_back( string(arr1[i]));
+	}
+
+	int result = ans.maxProduct(vec1);
+	cout << result << endl;
 }
