0f414fb57004b9b2154fb1a0b408e142caa82c3a
Ashish Misra <amisra@salesforce.com>
2016-10-10 14:25:30 -0700

    Implementation 2 for problem 0064. Still running into the same time-out problem in LeetCode.

--- src/incomplete/0064-minimum-path.cpp
+++ src/incomplete/0064-minimum-path.cpp
@@ -15,17 +15,18 @@ class Solution {
 		int minPathSum(vector<vector<int> > &grid);
 
 	private:
-		int minPathSum(vector<vector<int> > &grid, int i, int j);
+		void minPathSum(vector<vector<int> > &grid, int i, int j, int path_sum, int &min_path);
 };
 
 
-int Solution::minPathSum(vector<vector<int> > &grid, int i, int j) {
+void Solution::minPathSum(vector<vector<int> > &grid, int i, int j, int path_sum, int &min_path) {
 
-	// Reached our path.
-	if (	(i == (grid.size() - 1)) && 
-			(j == (grid[0].size() - 1))
-		) {
-		return grid[i][j];
+	if ((i == (grid.size()-1)) && 
+		(j == (grid[0].size()-1)) 
+	) {
+		int new_path_sum = path_sum + grid[i][j];
+		min_path = (new_path_sum < min_path) ? new_path_sum : min_path;
+		return;
 	}
 
 	// Argument check.
@@ -33,19 +34,20 @@ int Solution::minPathSum(vector<vector<int> > &grid, int i, int j) {
 		throw 0;
 	}
 
-	int result = INT_MAX;
-	int tmp_result;
+	int new_path_sum = path_sum + grid[i][j];
+
+	// Check if we should continue down our current path?
+	if (new_path_sum > min_path) {
+		return;
+	}
 
 	if (i < (grid.size() - 1)) {
-		result = minPathSum(grid, (i+1), j) + grid[i][j];
+		minPathSum(grid, (i+1), j, new_path_sum, min_path);
 	}
 
 	if (j < (grid[0].size() - 1)) {
-		tmp_result = minPathSum(grid, i, (j+1)) + grid[i][j];
-		result = (result > tmp_result) ? tmp_result : result;
+		minPathSum(grid, i, (j+1), new_path_sum, min_path);
 	}
-
-	return result;
 }
 
 int Solution::minPathSum(vector<vector<int> > &grid) {
@@ -55,7 +57,10 @@ int Solution::minPathSum(vector<vector<int> > &grid) {
 		return 0;
 	}
 
-	return minPathSum(grid, 0, 0);
+	int result = INT_MAX;
+	minPathSum(grid, 0, 0, 0, result);
+
+	return result;
 }
 
 int main(int argc, const char **argv) {
