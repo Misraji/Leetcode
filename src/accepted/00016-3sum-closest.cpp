835d7b2ae238e4a36852638227dac4b7f32cd9ce
Ashish Misra <developer.misra@gmail.com>
2023-01-05 17:28:54 -0800

    Implemented brute force solution for 00016-3sum-closest problem

--- src/accepted/00016-3sum-closest.cpp
+++ src/accepted/00016-3sum-closest.cpp
@@ -2,8 +2,26 @@
 
 #include <iostream>
 
-int Solution::threeSumClosest(const std::vector<int>& nums, int target) {
-	std::cerr << "Not implemented" << std::endl;
+int Solution::bruteforce(const std::vector<int>& nums, int target) {
+	int current = 1000000;
+	int current_diff = abs(target - current);
 
+	for (size_t i = 0; i < nums.size(); i++) {
+		for (size_t j = i+1; j < nums.size(); j++) {
+			for (size_t k = j+1; k < nums.size(); k++) {
+				const int sum = nums[i] + nums[j] + nums[k];
+				const int diff = abs(target - sum);
+				if (diff < current_diff) {
+					current = sum;
+					current_diff = diff;
+				}
+			}
+		}
+	}
+	return current;
+}
+
+int Solution::threeSumClosest(const std::vector<int>& nums, int target) {
+	// return bruteforce(nums, target);
 	return -1;
 }
--- src/accepted/00016-3sum-closest.h
+++ src/accepted/00016-3sum-closest.h
@@ -6,6 +6,9 @@
 class Solution {
 	public:
 		int threeSumClosest(const std::vector<int>& nums, int target);
+
+	private:
+		int bruteforce(const std::vector<int>& nums, int target);
 };
 
 #endif  // SRC_ACCEPTED_00016_3SUM_CLOSEST_H
